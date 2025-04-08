b0daf72f1cc0ce95b39e16fa02578a80ec3f700a
xlg08 <1146890973@qq.com>
2025-04-08 19:56:52 +0800

    LK

---
+++ Number_2/No_2_1.py
@@ -0,0 +1,11 @@
+'''
+    @User: LG
+    @Auther: http://www.xlonggang.cn
+    @Date: 2025/4/8 0008 19:56 
+    @Description: 
+    @Project ：LeetCode 
+    @File    ：No_2_1.py
+    @version: 1.0
+'''
+
+
