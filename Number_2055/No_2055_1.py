56d2b1d1f3747acb91c4b378f83221c6656f22d9
xlg08 <1146890973@qq.com>
2026-03-08 22:45:49 +0800

    LK

--- Number_2055/No_2055_1.py
+++ Number_2055/No_2055_1.py
@@ -3,8 +3,13 @@ from typing import List
 
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+
         n = len(s)  # 桌子长度
-        preSum, sum = [0] * n, 0  # 前缀和数组， 盘子数量
+
+        # 1. 构建前缀和数组 preSum，用于快速计算任意区间内的盘子总数
+        preSum, sum = [0] * n, 0  # 前缀和数组(表示从开头到位置 i 有多少个盘子('*'))， 累积的盘子数量
+
+        # 左侧最近的蜡烛位置 left
         left, l = [0] * n, -1           # 左侧最近蜡烛位置，
         for i, ch in enumerate(s):      # 遍历桌子数组
             if ch == '*':
@@ -14,17 +19,20 @@ class Solution:
             preSum[i] = sum
             left[i] = l
 
+        # 右侧最近的蜡烛位置 right
         right, r = [0] * n, -1
         for i in range(n - 1, -1, -1):
             if s[i] == '|':
                 r = i
             right[i] = r
 
+        # 处理查询
         ans = [0] * len(queries)
         for i, (x, y) in enumerate(queries):
             x, y = right[x], left[y]
             if x >= 0 and y >= 0 and x < y:
                 ans[i] = preSum[y] - preSum[x]
+
         return ans
 
 
--- Number_455/No_455_1.py
+++ Number_455/No_455_1.py
@@ -6,22 +6,22 @@ class Solution:
 
         # 变量 index_g 和 index_s 分别指向 g、s 初始位置
         index_g, index_s = 0, 0
-        res = 0         # 使用res保存结果  ->   满足孩子数
+        res = 0  # 使用res保存结果  ->   满足孩子数
 
         while index_g < len(g) and index_s < len(s):
-            if g[index_g] <= s[index_s]:            # 孩子的胃口数 <= 饼干尺寸
+            if g[index_g] <= s[index_s]:  # 孩子的胃口数 <= 饼干尺寸
                 res += 1
-                index_g += 1
-                index_s += 1
+                index_g += 1  # 该孩子的胃口已经被满足(换下一个孩子)
+                index_s += 1  # 饼干数(换下一个饼干)
             else:
-                index_s += 1        # 饼干数
+                index_s += 1  # 饼干数(换下一个大尺寸的饼干)
 
-        return res
+        return res  # 返回被满足的孩子数
 
 
 if __name__ == '__main__':
-    g = [2, 2]      # 孩子胃口
-    s = [1, 1, 1]       # 饼干尺寸
+    g = [2, 2]  # 孩子胃口
+    s = [1, 1, 1]  # 饼干尺寸
 
     so = Solution()
     print(so.findContentChildren(g, s))
@@ -30,5 +30,3 @@ if __name__ == '__main__':
     s = [1, 2, 3]
 
     print(so.findContentChildren(g, s))
-
-
